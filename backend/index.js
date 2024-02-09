6f064c29e6a0cea9e0ef69269d9e5d121688b9e0
FerryFL01 <ferryfebrian.lim@gmail.com>
2024-02-09 17:06:29 +0700

    m

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,49 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const cors = require('cors') // Import cors middleware
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const progressRoutes = require('./routes/progress')
-const reviewRoutes = require('./routes/review')
-const userRoutes = require('./routes/user')
+const progressRoutes = require('./routes/progress');
+const reviewRoutes = require('./routes/review');
+const userRoutes = require('./routes/user');
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
-// Enable CORS for all routes
-
-
+// Enable CORS for specific routes
 const corsConfig = {
     origin: "*",
-    credential: true,
-    methods: ["GET","POST","PATCH","DELETE"]
-}
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"]
+};
 
-app.options("", cors(corsConfig))
-app.use(cors(corsConfig))
+app.use('', cors(corsConfig));
 
-// app.use(cors({
-//     origin: 'https://one-percent-lime.vercel.app'
-// }));
-
-app.get("/", (req,res)=>{
-    res.json({hello})
-})
+app.get("/", (req, res) => {
+    res.json({ hello: 'world' });
+});
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    console.log(req.path, req.method);
+    next();
+});
 
 // Routes
-app.use('/api/progress', progressRoutes)
-app.use('/api/review', reviewRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/progress', progressRoutes);
+app.use('/api/review', reviewRoutes);
+app.use('/api/user', userRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // Listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('Connected to db and listening on port', process.env.PORT)
-        })
+            console.log('Connected to db and listening on port', process.env.PORT);
+        });
     })
     .catch((error) => {
-        console.log(error)
-    })
+        console.log(error);
+    });
